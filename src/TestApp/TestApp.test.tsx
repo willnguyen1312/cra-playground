16f5770436cded5e0becd8625d9ee2cd72edf83d
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-08-14 12:17:19 +0700

    Cool

--- src/TestApp/TestApp.test.tsx
+++ src/TestApp/TestApp.test.tsx
@@ -1,48 +1,52 @@
 // app.test.js
-import React from 'react'
-import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
-import { render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import { LocationDisplay, App } from './TestApp'
+import React from "react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { LocationDisplay, App } from "./TestApp";
 
-test('full app rendering/navigating', () => {
-  const history = createMemoryHistory()
+test("full app rendering/navigating", () => {
+  const history = createMemoryHistory();
   const { container, getByText } = render(
     <Router history={history}>
       <App />
     </Router>
-  )
+  );
   // verify page content for expected route
   // often you'd use a data-testid or role query, but this is also possible
-  expect(container.innerHTML).toMatch('You are home')
+  expect(container.innerHTML).toMatch("You are home");
 
-  fireEvent.click(getByText(/about/i))
+  fireEvent.click(getByText(/about/i));
 
   // check that the content changed to the new page
-  expect(container.innerHTML).toMatch('You are on the about page')
-})
+  expect(container.innerHTML).toMatch("You are on the about page");
+});
 
-test('landing on a bad page shows 404 page', () => {
-  const history = createMemoryHistory()
-  history.push('/some/bad/route')
-  const { getByRole } = render(
-    <Router history={history}>
-      <App />
-    </Router>
-  )
-  expect(getByRole('heading')).toHaveTextContent('404 Not Found')
-  console.log(history.location)
-})
+test("landing on a bad page shows 404 page", () => {
+  const _history = createMemoryHistory();
+  _history.push("/some/bad/route/ahaha");
+  const stuff = {
+    ...render(
+      <Router history={_history}>
+        <App />
+      </Router>
+    ),
+    history: _history,
+  };
 
-test('rendering a component that uses withRouter', () => {
-  const history = createMemoryHistory()
-  const route = '/some-route'
-  history.push(route)
+  const { getByRole, history } = stuff;
+  expect(getByRole("heading")).toHaveTextContent("404 Not Found");
+});
+
+test("rendering a component that uses withRouter", () => {
+  const history = createMemoryHistory();
+  const route = "/some-route";
+  history.push(route);
   const { getByTestId } = render(
     <Router history={history}>
       <LocationDisplay />
     </Router>
-  )
-  expect(getByTestId('location-display')).toHaveTextContent(route)
-})
+  );
+  expect(getByTestId("location-display")).toHaveTextContent(route);
+});
