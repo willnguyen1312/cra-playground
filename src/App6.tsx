5c34553e01c8b51cfccdf1ea334cc53c4806cdcd
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-02-01 22:50:57 +0700

    oho

---
+++ src/App6.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+// import mp3 from "./assets/sample.mp3";
+
+export default function App6() {
+  const [src, setSrc] = useState(localStorage.getItem("src") as string);
+  //   useEffect(() => {
+  //     fetch("/sample.mp3")
+  //       .then((res) => res.blob())
+  //       .then((blob) => {
+  //         const reader = new FileReader();
+  //         reader.onload = () => {
+  //           setSrc(reader.result as string);
+  //         };
+  //         reader.readAsDataURL(blob);
+  //       });
+  //   }, []);
+
+  //   localStorage.setItem("src", src);
+
+  return (
+    <div>
+      <h1>Hello there</h1>
+      <audio controls src={src} autoPlay />
+      {/* <video controls src={src} /> */}
+    </div>
+  );
+}
