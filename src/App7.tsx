2f5b25580015b325669a2b328bbf956d4ab16b49
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-02-21 06:48:42 +0700

    Add polyfill

--- src/App7.tsx
+++ src/App7.tsx
@@ -2,6 +2,24 @@ import React from "react";
 
 type MediaPropertiesValue = number | string | boolean;
 
+(function () {
+  if (typeof window.CustomEvent === "function") return false;
+
+  function CustomEvent(event, params) {
+    params = params || { bubbles: false, cancelable: false, detail: null };
+    var evt = document.createEvent("CustomEvent");
+    evt.initCustomEvent(
+      event,
+      params.bubbles,
+      params.cancelable,
+      params.detail
+    );
+    return evt;
+  }
+
+  (window as any).CustomEvent = CustomEvent;
+})();
+
 const hijackMediaElement = (
   mediaElement: HTMLAudioElement | HTMLVideoElement
 ) => {
